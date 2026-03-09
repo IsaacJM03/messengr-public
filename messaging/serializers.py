0e88bbdf812db2e3034d0050c9cbeae2e87bd5b3
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2026-03-09 22:58:25 +0300

    added minor improvements

--- messaging/serializers.py
+++ messaging/serializers.py
@@ -6,10 +6,12 @@ class MessageSerializer(serializers.ModelSerializer):
      model = Message
      fields = "__all__"
 
- def create(self, validated_data):
-     return Message.objects.create(
-         sender=validated_data['sender'], #noticed swapped sender and recipient 
-         recipient=validated_data['recipient'],
-         text=validated_data['text'],
-         language=validated_data['language']
-     )
+     def validate(self, data):
+        sender = data.get("sender")
+        recipient = data.get("recipient")
+        if sender and recipient and sender == recipient:
+            raise serializers.ValidationError("sender and recipient must be different")
+        return data
+
+     def create(self, validated_data):
+        return Message.objects.create(**validated_data)
